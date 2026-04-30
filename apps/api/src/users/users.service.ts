fd20a0368dd65bd61829007763c82d7e1fc43317
chaitanya medavarapu <mchaitanya1118@gmail.com>
2026-05-01 00:01:35 +0530

    fix: remove remaining legacy bcrypt imports

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, ConflictException } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
-import * as bcrypt from "bcrypt";
+import * as bcrypt from "bcryptjs";
 import { UserRole } from "@prisma/client";
 
 @Injectable()
